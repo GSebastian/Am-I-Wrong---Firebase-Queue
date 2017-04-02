d7193387fff552411455069357ba712f1af55832
Sebastian <sebastian@ghetu.net>
2017-04-02 02:58:28 +0100

    Added support for reporting and tasks are now handled by 2 queues

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var admin = require('firebase-admin');
 var Queue = require('firebase-queue');
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
 
 var serviceAccount = require('./service-account-key.json');
 var FirebaseManager = require('./firebase-manager');
@@ -10,6 +12,8 @@ class MainClass {
     constructor() {
         this.app = express();
 
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
             databaseURL: "***REMOVED***"
@@ -18,6 +22,13 @@ class MainClass {
         this.firebaseManager = new FirebaseManager(this.ROOT_NODE);
 
         this.queueRef = admin.database().ref('/Queue');
+
+        this.transporter = nodemailer.createTransport(mg({
+            auth: {
+                api_key: '***REMOVED***',
+                domain: '***REMOVED***'
+            }
+        }));
 
         this.startQueues();
     }
@@ -37,13 +48,25 @@ class MainClass {
 
     startQueues() {
         let votesQueueOptions = {
-            'numWorkers': 1
+            'numWorkers': 1,
+            'specId': 'vote'
         };
         this.votesQueue = new Queue(this.queueRef, votesQueueOptions, (data, progress, resolve, reject) => {
             // Read and process task data
             console.log(data);
 
             this.handleVote(data, progress, resolve, reject);
+        });
+
+        let reportsQueueOptions = {
+            'numWorkers': 5,
+            'specId': 'report'
+        };
+        this.reportsQueue = new Queue(this.queueRef, reportsQueueOptions, (data, progress, resolve, reject) => {
+            // Read and process task data
+            console.log(data);
+
+            this.handleReport(data, progress, resolve, reject);
         });
     }
 
@@ -72,6 +95,31 @@ class MainClass {
                     }
                 });
             }
+        });
+    }
+
+    handleReport(task, progress, resolve, reject) {
+
+        let questionId = task.questionId;
+
+        this.firebaseManager.fetchQuestion(questionId, questionData => {
+            let question = questionData.text;
+
+            let mailOptions = {
+                from: '"AIW Report Bot" <noreply@amiwrong.com>', // sender address
+                to: 'seb@roboto.studio', // list of receivers
+                subject: questionId, // Subject line
+                text: 'Reported for spam or offensive content \n\n Question: ' + question, // plain text body
+            };
+
+            this.transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    reject();
+                    return console.log(error);
+                }
+                console.log('Report mail sent');
+                resolve();
+            });
         });
     }
 
